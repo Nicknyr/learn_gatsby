8102019ae26560fec95ff0a32e9b083e25b85d18
Nicknyr <nick@Nicks-MacBook-Pro.local>
2020-08-11 08:44:48 -0400

    Basic Gatsby site with two basic pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+
+const AboutPage = () => (
+    <Layout>
+        <h1>About Us</h1>
+        <p>ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis eu volutpat odio facilisis mauris sit amet massa vitae tortor condimentum lacinia quis vel eros donec ac odio tempor orci</p>
+    </Layout>
+)
+   
+
+export default AboutPage;
